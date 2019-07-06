029bce71f5f12f1e358cf5cbcd316925c0f0509b
Fernando Britto <me@fernandobritto.com.br>
2019-07-06 14:47:50 -0300

    FrontController

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@ require __DIR__. '/../vendor/autoload.php';
 $url = substr($_SERVER['REQUEST_URI'], 1);
 $url = explode('/', $url);
 
-$controller = isset($url[0]) ? $url[0] : 'page';
+$controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
 
-//print($controller);
+$controller = ucfirst($controller). 'Controller';
+
+print $controller;
 
