239d75603f2c6b7475b40da4c6231a5f2ce9fdc9
Fernando Britto <me@fernandobritto.com.br>
2019-07-07 09:31:38 -0300

    Controller file

--- public/index.php
+++ public/index.php
@@ -5,8 +5,11 @@ $url = substr($_SERVER['REQUEST_URI'], 1);
 $url = explode('/', $url);
 
 $controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
+$action = isset($url[1]) && $url[1] ? $url[1] : 'index';
 
 $controller = ucfirst($controller). 'Controller';
 
-print $controller;
+$execute = new $controller();
+print $execute->index();
+
 
