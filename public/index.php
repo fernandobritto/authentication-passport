7b78d0c71e906ad576941432b1e80e588698450d
Fernando Britto <me@fernandobritto.com.br>
2019-07-07 09:43:11 -0300

    PageController

--- public/index.php
+++ public/index.php
@@ -1,15 +1,23 @@
 <?php
 require __DIR__. '/../vendor/autoload.php';
 
+
+
+
+
 $url = substr($_SERVER['REQUEST_URI'], 1);
 $url = explode('/', $url);
 
 $controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
 $action = isset($url[1]) && $url[1] ? $url[1] : 'index';
 
-$controller = ucfirst($controller). 'Controller';
+$controller = "Code\Controller\\".ucfirst($controller). 'Controller';
 
-$execute = new $controller();
-print $execute->index();
+$response = call_user_func_array([new $controller,$action], []);
+
+//$execute = new PageController();
+//print $execute->index();
+
+print $response;
 
 
---
+++ src/Controller/PageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Code\Controller;
+
+class PageController
+{
+	public function index()
+	{
+		return '<h1>PageController</h1>';
+	}
+}
