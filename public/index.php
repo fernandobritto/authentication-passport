0bcac5985658cde4d4c7d272770c6a624a437aca
Fernando Britto <me@fernandobritto.com.br>
2019-07-10 21:09:25 -0300

    Initial commit

--- public/index.php
+++ public/index.php
@@ -2,3 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$url = substr($_SERVER['REQUEST_URI'], 1);
+$url = explode('/', $url);
+
+$controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
+
+//print($controller);
