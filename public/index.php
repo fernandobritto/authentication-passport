96884e053afbf16a75d9c31f5465465bf8751a8c
Fernando Britto <me@fernandobritto.com.br>
2019-07-11 21:19:03 -0300

    FrontController

--- public/index.php
+++ public/index.php
@@ -6,5 +6,11 @@ $url = substr($_SERVER['REQUEST_URI'], 1);
 $url = explode('/', $url);
 
 $controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
+$action     = isset($url[1]) && $url[1] ? $url[1] : 'index';
 
-//print($controller);
+
+$controller = ucfirst($controller).'Controller';
+
+print 'Controller Default: '.$controller;
+print "<br>";
+print 'Metodo Default: '.$action;
