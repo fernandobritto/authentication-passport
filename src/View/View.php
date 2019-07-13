bcc6803574af5ef4a78d0922b3d7508b24dab844
Fernando Britto <me@fernandobritto.com.br>
2019-07-13 10:01:51 -0300

    Update View.php

--- src/View/View.php
+++ src/View/View.php
@@ -5,9 +5,23 @@ namespace View;
 class View
 {
 	private $view;
+	private $data = [].
 
 	public function __construct($view)
 	{
 		$this->view = $view;
 	}
+
+	public function __set($index, $value)
+	{
+
+	}
+
+	public function __get($index)
+	{
+		
+	}
+
+
+
 }
