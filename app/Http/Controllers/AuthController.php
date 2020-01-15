1c7b1b23e874186200f92223d40bf159e4eab3ff
Fernando Britto <me@fernandobritto.com.br>
2020-01-15 21:38:50 -0200

    Criação do controlador - AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
+
+}
