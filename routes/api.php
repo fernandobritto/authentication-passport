ec1406c3b88c99b5d0aad2ed1c19131450fcca4c
Fernando Britto <me@fernandobritto.com.br>
2020-01-18 15:44:58 -0200

    Configuração das Rotas

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('auth')->group(function (){
     Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+
+    Route::middleware('auth:api')->group(function(){
+        Route::post('logout', 'AuthController@logout');
+    });
 });
+
+Route::get('products', 'ProductsController@index')
+    ->middleware('auth:api');
+
