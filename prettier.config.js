180fa553e45f6c800d20b2d8ddd9d6da15190ba2
Fernando Britto <fernando@COLTWFFHVXT3>
2025-03-07 19:00:07 -0300

    Initial commit

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: false,
-  tabWidth: 2,
-  useTabs: false,
-  printWidth: 120,
-  singleQuote: true,
-  trailingComma: 'none'
-}
--- scripts/create-bucket.sh
+++
@@ -1,9 +0,0 @@
-BUCKET_NAME=authentication-passport-bucket
-
-aws \
-  s3 mb s3://$BUCKET_NAME \
-  --endpoint-url=http://localhost:4566
-
-aws \
-  s3 ls \
-  --endpoint-url=http://localhost:4566
